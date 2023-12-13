c256a8cceb878fc82b3104dad58e9341b62a93c2
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-12-13 18:28:02 +0530

    Update 📝_Appointment.py

--- pages/📝_Appointment.py
+++ pages/📝_Appointment.py
@@ -22,6 +22,7 @@ appointment_date = st.date_input(
 
 current_date = dt.date.today()
 current_time = dt.datetime.now().time()
+st.header(current_time)
 
 if doctor_name and appointment_date:
     doctor_id = get_doctors_id(db_connection, doctor_name)
