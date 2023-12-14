6e685680029a3141717c432b0c31abf878438833
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-12-14 12:53:49 +0530

    Add files via upload

---
+++ pages/🙍‍♂️_Patient Information.py
@@ -0,0 +1,179 @@
+import streamlit as st
+from datetime import datetime
+from secret.credentials import *
+from SQL_connection import create_server_connection, execute_query
+from utils import get_disease_id
+from streamlit_extras.switch_page_button import switch_page
+
+if 'current_page' not in st.session_state:
+    st.session_state['current_page'] = 'Patient Information'
+
+st.subheader('Patient Information')
+patient_id = int(st.text_input('Patient ID', '0'))
+first_name = st.text_input('First Name', '').strip()
+last_name = st.text_input('Last Name', '').strip()
+gender = st.selectbox('Gender', ['', 'Male', 'Female', 'Other'])
+mobile = st.text_input('Mobile', '').strip()
+email = st.text_input('Email', '').strip()
+dob = st.date_input('Date of Birth', min_value=datetime(
+    1900, 1, 1), max_value=datetime.now(), key='dob')
+marital_status = st.selectbox(
+    'Marital Status', ['', 'Single', 'Married', 'Divorced', 'Widowed'])
+
+st.subheader(" ")
+
+st.subheader('Emergency Contact Information')
+ec_email = st.text_input('Emergency Contact Email', '').strip()
+ec_mobile = st.text_input('Emergency Contact Mobile Number', '').strip()
+ec_relationship = st.text_input('Relationship to Patient', '').strip()
+
+st.subheader(" ")
+
+st.subheader('Patient Address Information')
+address = st.text_input('Address', '').strip()
+city = st.text_input('City', '').strip()
+zipcode = st.text_input('ZipCode', '').strip()
+state = st.text_input('State', '').strip()
+country = st.text_input('Country', '').strip()
+
+st.subheader(" ")
+
+st.subheader('Patient Medical Background Information')
+weight = st.number_input('Weight (in kg)', min_value=0.0, format='%.2f')
+blood_group = st.selectbox(
+    'Blood Group', ['', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
+tobacco_usage = st.checkbox('Tobacco Usage')
+alcohol_intake = st.checkbox('Alcohol Intake')
+allergies = st.text_area('Allergies')
+st.header(allergies)
+is_diabetic = st.checkbox('Is Diabetic')
+is_having_bp = st.checkbox('Is Having Blood Pressure')
+
+st.subheader(" ")
+st.subheader('Family Medical History')
+
+# Define your diseases
+diseases = [
+    'Mental Health Disorder',
+    'Colon Cancer',
+    'Prostate/Uterine Cancer',
+    'Breast Cancer',
+    'Lung Cancer',
+    'Diabetes',
+    'High Blood Pressure',
+    'High Cholesterol',
+    'Heart Disease/Kidney Disease',
+    'Alcohol Issues'
+]
+
+col1, col2 = st.columns(2)
+
+checked_diseases = []
+
+with col1:
+    for disease in diseases[:5]:
+        if st.checkbox(disease, key=disease):
+            checked_diseases.append(disease)
+
+with col2:
+    for disease in diseases[5:]:
+        if st.checkbox(disease, key=disease):
+            checked_diseases.append(disease)
+
+
+st.subheader(" ")
+st.subheader('Health Insurance Information')
+health_insurance_id = int(st.text_input('Health Insurance ID', '0'))
+provider_name = st.text_input('Provider Name', '').strip()
+coverage_plan = st.text_input('Coverage Plan', '').strip()
+coverage_is_under = st.text_input('Coverage is Under', '').strip()
+
+
+st.subheader(" ")
+submit_button = st.button(label='Submit')
+
+db_connection = create_server_connection(host, username, password, database)
+
+
+if submit_button:
+    if not all([patient_id, first_name, last_name, gender, mobile, email, dob, marital_status, ec_email, ec_mobile, ec_relationship, address, city, zipcode, state, country, weight, blood_group]):
+        st.error('Please fill out all fields.')
+    elif patient_id == 0:
+        st.error('Please Enter Valid Patient ID.')
+    else:
+        patient_tuple = (patient_id, first_name, last_name, gender,
+                         mobile, email, dob, marital_status)
+        emergency_contact_tuple = (
+            patient_id, ec_email, ec_mobile, ec_relationship)
+
+        patient_address_tuple = (
+            patient_id, address, city, zipcode, state, country)
+
+        patient_medical_background_tuple = (
+            patient_id, weight, blood_group, tobacco_usage, alcohol_intake, allergies, is_diabetic, is_having_bp)
+
+        health_insurance_data = (
+            patient_id, health_insurance_id, provider_name, coverage_plan, coverage_is_under)
+
+        insert_patient_query = f"""
+                                INSERT INTO Patient (PatientID,FirstName, LastName, Gender, Mobile, Email, DOB, MaritalStatus)
+                                            VALUES ('{patient_tuple[0]}', '{patient_tuple[1]}', '{patient_tuple[2]}',
+                                                    '{patient_tuple[3]}', '{patient_tuple[4]}', '{patient_tuple[5]}', '{patient_tuple[6]}', '{patient_tuple[7]}');"""
+
+        insert_emergency_contact_query = f"""
+                                            INSERT INTO EmergencyContact (PatientID,Email, MobileNumber, RelationshipToPatient)
+                                                        VALUES ('{emergency_contact_tuple[0]}', '{emergency_contact_tuple[1]}', '{emergency_contact_tuple[2]}','{emergency_contact_tuple[3]}');
+                                        """
+
+        insert_patient_address_query = f"""
+                                            INSERT INTO PatientAddress (PatientID, Address, City, ZipCode, State, Country)
+                                                        VALUES ('{patient_address_tuple[0]}', '{patient_address_tuple[1]}', '{patient_address_tuple[2]}',
+                                                        '{patient_address_tuple[3]}', '{patient_address_tuple[4]}', '{patient_address_tuple[5]}');
+                                        """
+
+        insert_patient_medical_background_query = f"""
+                                            INSERT INTO PatientMedicalBackground (PatientID, Weight, BloodGroup, TobaccoUsage, AlcoholIntake, Allergies, IsDiabetic, IsHavingBP)
+                                                        VALUES ('{patient_medical_background_tuple[0]}', '{patient_medical_background_tuple[1]}', '{patient_medical_background_tuple[2]}',
+                                                                    {patient_medical_background_tuple[3]}, {patient_medical_background_tuple[4]}, '{patient_medical_background_tuple[5]}',
+                                                                    {patient_medical_background_tuple[6]}, {patient_medical_background_tuple[7]});
+                                        """
+
+    db_connection = create_server_connection(
+        host, username, password, database)
+
+    execute_query(db_connection, insert_patient_query)
+    execute_query(db_connection, insert_emergency_contact_query)
+    execute_query(db_connection, insert_patient_address_query)
+    execute_query(db_connection, insert_patient_medical_background_query)
+
+    for disease_name in checked_diseases:
+        disease_id = get_disease_id(db_connection, disease_name)
+        if disease_id is not None:
+            insert_query = f"""
+                INSERT INTO PatientFamilyMedicalBackground (PatientID, DiseaseID)
+                        VALUES ({patient_id}, {disease_id});
+                    """
+            execute_query(db_connection, insert_query)
+
+    if all([patient_id, health_insurance_id, provider_name, coverage_plan, coverage_is_under]):
+        insert_health_insurance_query = f"""
+                                            INSERT INTO HealthInsurance (PatientID, HealthInsuranceID,ProviderName, CoveragePlan, CoverageIsUnder)
+                                                        VALUES ('{health_insurance_data[0]}', '{health_insurance_data[1]}', '{health_insurance_data[2]}',
+                                                                    {health_insurance_data[3]}, {health_insurance_data[4]});
+                                        """
+
+        execute_query(db_connection, insert_health_insurance_query)
+
+
+st.title(" ")
+st.header(" ")
+
+col1, spacer1, col2 = st.columns(
+    [1, 1.5, 1])
+
+
+with col2:
+    patient_info_back_button = st.button(label='Go Back to Patient Page')
+    if patient_info_back_button:
+        st.session_state['current_page'] = "Patient"
+        switch_page("Patient")
---
+++ pages/🧾_Patient_Prescription.py
@@ -0,0 +1,111 @@
+import streamlit as st
+from datetime import datetime
+from secret.credentials import *
+from SQL_connection import create_server_connection, execute_query
+from utils import *
+import pandas as pd
+from streamlit_extras.switch_page_button import switch_page
+
+st.title('Doctor Prescription')
+patient_id = int(st.text_input('Enter Patient ID', '0'))
+st.subheader(" ")
+
+db_connection = create_server_connection(
+    host, username, password, database)
+
+if check_patient_exists(db_connection, patient_id):
+    if patient_id:
+        prescription_details = get_patient_prescriptions(
+            db_connection, patient_id)
+
+        if prescription_details:
+            st.subheader('Latest Appointment and Prescription Details')
+            appointments_df = pd.DataFrame(prescription_details, columns=[
+                'Appointment ID', 'Appointment Date', 'Prescribed Date', 'Medicine Name', 'Medicine Dosage', 'Medicine Duration', 'Prescription Status'])
+
+            appointments_df = appointments_df.to_html(
+                index=False, border=0)
+
+            st.title("")
+
+            st.markdown(appointments_df, unsafe_allow_html=True)
+
+            st.title("")
+            st.header("")
+
+            continue_previous = st.checkbox(
+                'Continue with the previous prescription?', True)
+
+            if continue_previous:
+                st.info("Continuing with the previous Prescription")
+
+            else:
+                update_prescription_status(
+                    db_connection, patient_id)
+                st.success(
+                    "Previous prescriptions have been discontinued.")
+
+        medicine_list = get_medicines_list(db_connection)
+
+        medicine_names = ['']+[item[1] for item in medicine_list]
+
+        prescription_entries = []
+
+        st.title("")
+        st.header("")
+
+        with st.form(key='prescription_form'):
+            st.write("Enter Prescription Details")
+            # Create multiple input sets for medicine name, dosage, and duration
+            for i in range(5):  # Adjust the range to allow for more or fewer entries
+                col1, col2, col3 = st.columns(3)
+                with col1:
+                    medicine_name = st.selectbox(
+                        f'Medicine {i+1}', medicine_names, key=f'medicine_{i}')
+                with col2:
+                    dosage = st.text_input(
+                        f'Dosage {i+1}', key=f'dosage_{i}')
+                with col3:
+                    duration = st.text_input(
+                        f'Duration {i+1}', key=f'duration_{i}')
+                # Add the details to the prescription_entries list
+                prescription_entries.append(
+                    {'medicine_name': medicine_name, 'dosage': dosage, 'duration': duration})
+
+            submit_button = st.form_submit_button(
+                label='Submit Prescription')
+
+        if submit_button:
+
+            for entry in prescription_entries:
+                if entry['medicine_name'] and entry['dosage'] and entry['duration']:
+                    medicine_id = get_medicine_id(
+                        db_connection, entry['medicine_name'])
+
+                    prescription_data = (
+                        patient_id, medicine_id, entry['dosage'], entry['duration'], prescription_details[0][0])
+
+                    insert_patient_precription(
+                        db_connection, prescription_data)
+
+            st.success("Prescription added successfully.")
+
+    else:
+        st.warning(
+            'No Appointments and Prescriptions found for this patient.')
+else:
+    st.warning("The Patient Doesn't Exsist")
+
+
+st.title(" ")
+st.header(" ")
+
+col1, spacer1, col2 = st.columns(
+    [1, 1.5, 1])
+
+with col2:
+    patient_prescription_back_button = st.button(
+        label='Go Back to Doctor Page')
+    if patient_prescription_back_button:
+        st.session_state['current_page'] = "Doctor"
+        switch_page("Doctor")
