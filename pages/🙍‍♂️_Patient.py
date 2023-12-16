457e78346e28601cfd1367b34b2669a9ea9b052f
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-12-16 12:21:51 +0530

    Update 🙍‍♂️_Patient.py

--- pages/🙍‍♂️_Patient.py
+++ pages/🙍‍♂️_Patient.py
@@ -75,9 +75,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Main title
-st.markdown("<h1>Welcome to Patient Dashboard</h1>",
-            unsafe_allow_html=True)
+col1, col2, col3 = st.columns([1.5, 6, 1.3])
+with col2:
+    st.markdown("<h1 style='text-align: center;'>Welcome to Patient Dashboard</h1>",
+                unsafe_allow_html=True)
 
 # Function to convert image file to base64
 
