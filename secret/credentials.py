ea95d0a0867c4a2e5f10706052d922148a5b3bcd
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-12-16 11:46:35 +0530

    Update credentials.py

--- secret/credentials.py
+++ secret/credentials.py
@@ -1,6 +1,8 @@
-host = "hospitalmanagement.cllpfni6u29t.ap-south-1.rds.amazonaws.com"
-port = 3306
-database = "HMS"
-username = "admin"
-password = "HANUMAN2004"
-mongodb_password = "hanuman2004"
+import streamlit as st
+
+host = st.secrets["host"]
+port = st.secrets["PORT"]
+database = st.secrets["HMS"]
+username = st.secrets["admin"]
+password = st.secrets["HANUMAN2004"]
+mongodb_password = st.secrets["hanuman2004"]
