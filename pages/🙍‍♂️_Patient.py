60592a29a3be54fd477e7dfcfb799c6034651315
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-12-16 12:26:06 +0530

    Update 🙍‍♂️_Patient.py

--- pages/🙍‍♂️_Patient.py
+++ pages/🙍‍♂️_Patient.py
@@ -77,10 +77,13 @@ st.markdown(
 
 col1, col2, col3 = st.columns([1.5, 6, 1.3])
 with col2:
-    st.markdown("<h1 style='text-align: center;'>Welcome to Patient Dashboard</h1>",
+    st.markdown("<h1 style='text-align: center;'>Welcome to Doctor Dashboard</h1>",
                 unsafe_allow_html=True)
 
-# Function to convert image file to base64
+with col3:
+    if st.button("Logout"):
+        st.session_state['current_page'] = 'home'
+        switch_page("app")
 
 
 def img_to_base64(image_path):
