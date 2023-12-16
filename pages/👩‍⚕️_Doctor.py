b6c0761d88fab91f527b66ac9498c7a3b83b89c4
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-12-16 12:22:27 +0530

    Update 👩‍⚕️_Doctor.py

--- pages/👩‍⚕️_Doctor.py
+++ pages/👩‍⚕️_Doctor.py
@@ -77,11 +77,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Main title
-st.markdown("<h1>Welcome to Doctor Dashboard</h1>",
-            unsafe_allow_html=True)
-
-# Function to convert image file to base64
+col1, col2, col3 = st.columns([1.5, 6, 1.3])
+with col2:
+    st.markdown("<h1 style='text-align: center;'>Welcome to Doctor Dashboard</h1>",
+                unsafe_allow_html=True)
 
 
 def img_to_base64(image_path):
