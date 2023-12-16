afcd85469898e4243d167c9774ee2e16523deee0
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-12-16 11:52:58 +0530

    Update credentials.py

--- secret/credentials.py
+++ secret/credentials.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 host = st.secrets["host"]
-port = st.secrets["PORT"]
-database = st.secrets["HMS"]
-username = st.secrets["admin"]
-password = st.secrets["HANUMAN2004"]
-mongodb_password = st.secrets["hanuman2004"]
+port = st.secrets["port"]
+database = st.secrets["database"]
+username = st.secrets["username"]
+password = st.secrets["password"]
+mongodb_password = st.secrets["mongodb_password"]
