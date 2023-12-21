00b601a40fe0040b6f9c61174b37ebff1dfab1e7
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-12-21 19:09:15 +0530

    Update 🤵_ADMIN.py

--- pages/🤵_ADMIN.py
+++ pages/🤵_ADMIN.py
@@ -80,7 +80,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-col1, col2, col3 = st.columns([1.5, 6, 1.3])
+col1, col2, col3 = st.columns([1.5, 6, 0.9])
 with col2:
     st.markdown("<h1 style='text-align: center;'>Welcome to ADMIN Dashboard</h1>",
                 unsafe_allow_html=True)
